d638735e5c180d420af8e500e9e5971a3d661d6a
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2023-12-09 20:46:26 +0100

    final finish

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
     // variable to handle the players symbols; x and o
     public static String symbol;
 
-    // variable to handle if player 1 or player 2
+    // variable to handle player names
     public static String player;
 
     public static boolean winnerStatus;
@@ -39,14 +39,16 @@ public class Main {
         // game loop that keeps track of the player order - if no winner, the game stops after ninth round
         for (int i = 0; i < 9; i++) {
 
+            // player 1
             if (i % 2 == 0) {
-                System.out.printf("Player 1: \n%s it's your turn\n\n", player1.getName());
-                player = "Player 1";
+                System.out.printf("\nPlayer 1: \n%s it's your turn\n\n", player1.getName());
+                player = player1.getName();
                 symbol = "x";
                 }
+            // player 2
             else {
-                System.out.printf("Player 2: \n%s it's your turn\n\n", player2.getName());
-                player = "Player 2";
+                System.out.printf("\nPlayer 2: \n%s it's your turn\n\n", player2.getName());
+                player = player2.getName();
                 symbol = "o";
                 }
 
@@ -68,31 +70,39 @@ public class Main {
         }
     }
 
-
+    /**
+     * This method prompts the user for a String input
+     *
+     * @param prompt message to user
+     * @return String input from user
+     */
     public static String userInputString (String prompt){
 
         // print prompt message
         System.out.printf(prompt);
 
-        // create scanner object
+        // create scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // returns input
+        // returns input from user
         return scanner.nextLine();
     }
 
 
     /**
-     * This method
+     * This method prompts the user for an int input.
+     * Afterward , the user's input is validated in two steps;
+     * First, it is checked whether an int has been entered. If yes, the user's input is further
+     * validated against the min and max values; otherwise, the user is prompted to enter again.
      *
-     * @param prompt
-     * @param min
-     * @param max
-     * @return
+     * @param prompt message to user
+     * @param min value
+     * @param max value
+     * @return int input from user
      */
     public static int userInputInt (String prompt,int min, int max) {
 
-        // create scanner object
+        // create scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -100,45 +110,57 @@ public class Main {
             // print prompt message
             System.out.printf("\n%s",prompt);
 
+            // if user input is an int
             if (scanner.hasNextInt()) {
 
+                // stores the user input as a local variable
                 int input = scanner.nextInt();
 
+                // if input is in between the min and max value
                 if (input >= min && input <= max) {
 
-                    // doMagic!
+                    // cleans buffer (doMagic!)
                     scanner.nextLine();
 
+                    // returns input from user
                     return input;
 
+                // if input isn't in between the min and max value
                 } else {
                     // error message - if input is less than min value or more than max value
                     System.out.printf("Error: please enter a number between %d and %d\n", min, max);
                 }
             }
+            // if user input isn't an int
             else {
                 // error message - if input isn't a number
                 System.out.printf("Invalid input. please enter a number between %d and %d\n", min, max);
 
-                //clean buffer
-                scanner.reset().next();
+                //clean buffer (doMagic!)
+                scanner.nextLine();
             }
         }
     }
 
     /**
-     * This method
+     * This method is the actual game.
+     * The user is shown the possible moves. Afterward, the user is prompted to enter their next move.
+     * Using a switch case, the user's entered value/next move is transformed into a player symbol in the
+     * corresponding field on the game board, and the user's move is removed from the game board.
+     * Finally, the game get printed with the latest move.
      *
-     * @param playerSymbol
-     * @return
+     * @param playerSymbol either "x" or "o"
      */
     public static void theGame (String playerSymbol){
 
+        // shows possible moves at the game board
         System.out.println("Possible moves:");
         printGameBoard();
 
+        // gets player move from user and stores it as a local variable
         int playerMove = userInputInt("Please enter your next move here: ", 1, 9);
 
+        // switch case to switch player move into a symbol at the gameArray and an empty space at the game board
         switch (playerMove) {
 
             case 1:
@@ -187,9 +209,8 @@ public class Main {
                 break;
         }
 
+        // prints the game with the latest move
         printGame();
-
-        //check3InARow(symbol);
     }
 
 
@@ -217,18 +238,23 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println("\n___________________________________");
     }
 
 
     /**
-     * This method checks if a player has 3 in a row.
+     * This method checks if a player has 3 equal symbols in a row.
+     * All rows are checked, all columns are checked, and finally, the two diagonals are checked.
+     * If a player has 3 in a row, the method returns "true," and if a player doesn't have 3 in a row,
+     * it returns "false."
      *
-     * @param playerSymbol
-     * @return
+     * @param playerSymbol either "x" or "o"
+     * @return a boolean
      */
     public static boolean check3InARow (String playerSymbol){
         System.out.println();
 
+        // for loop to check if there is 3 equal symbols in a row (either in one of the rows or one of the columns)
         for (int i = 0 ; i <= 4 ; i += 2) {
 
             // checks the 3 rows
@@ -252,9 +278,8 @@ public class Main {
             return true;
         }
 
+        // if there isn't 3 equal symbols in a row
         else
             return false;
     }
-
-
 }
